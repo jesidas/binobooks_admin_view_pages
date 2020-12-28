e8e45790b3e24eb2742cd1081615b39ba6c0477d
Yueyang Liu <550204424@qq.com>
2020-12-28 16:37:06 +0800

    finsih user list

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import router from './router'
 import './plugins/element.js'
 // import global css
 import './assets/css/global.css'
+// import ali font
+import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // must return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
